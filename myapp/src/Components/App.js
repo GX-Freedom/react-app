4f0eee98a27cb900098f2a3d9be6decf70b2b685
GX-Freedom <kjy911206@gmail.com>
2020-05-04 14:47:04 +0900

    update
    update

---
+++ myapp/src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Router from './Router';
+import GlobalStyled from './GlobalStyle';
+
+
+function App() {
+    return (
+        <>
+            <Router />
+            <GlobalStyled />
+        </>
+    );
+}
+
+export default App;
---
+++ myapp/src/Components/Routes/Home/HomeContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import HomePresenter from "./HomePresenter"
+
+export default class extends React.Component {
+    render() {
+
+        return (
+            <>
+                <div>Home</div>
+            </>
+        )
+    }
+}
